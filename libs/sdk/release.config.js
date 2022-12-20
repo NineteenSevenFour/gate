22dd5bdbf3b006058686b802edb086bf6eb0dde7
Bruno DUVAL <48152847+datatunning@users.noreply.github.com>
2022-12-20 16:55:20 +0100

    fix(ci): release pipeline (#33)
    Fixes #30
    
    Co-authored-by: Bruno DUVAL <bruno.duval@datatunning.com>

--- libs/sdk/release.config.js
+++ libs/sdk/release.config.js
@@ -1,53 +1,35 @@
 /* eslint-disable */
 
-const appName = 'sdk';
-const appPath = `libs/${appName}`;
+const libName = 'sdk';
+const libPath = `libs/${libName}`;
 const rootPath = '../..';
 
 module.exports = {
   extends: `${rootPath}/release.config.base.js`,
   name: appName,
-  pkgRoot: `${rootPath}/dist/${appPath}`, // should come from angular.cli
-  tagFormat: `${appName}-v${version}`,
+  pkgRoot: `${rootPath}/dist/${libPath}`, // should come from angular.cli
+  tagFormat: `${libName}-v${version}`,
   commitPaths: [
     `${rootPath}/package.json`,
     `${rootPath}/package-lock.json`,
     `*`, // anything in this directory
   ],
-  assets: [`${appPath}/README.md`, `${appPath}/CHANGELOG.md`],
+  assets: [`${libPath}/README.md`, `${libPath}/CHANGELOG.md`],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
       '@semantic-release/changelog',
       {
-        changelogFile: `${appPath}/CHANGELOG.md`,
+        changelogFile: `${libPath}/CHANGELOG.md`,
       },
     ],
-    // [
-    //   '@semantic-release/github',
-    //   {
-    //     assets: [
-    //       {
-    //         path: `${appPath}/**`,
-    //         name: `${appName}-${nextRelease.gitTag}.zip`,
-    //         label: `${appName}-(${nextRelease.gitTag})`,
-    //       },
-    //     ],
-    //     successComment:
-    //       "This ${issue.pull_request ? 'pull request' : 'issue'} is included in version ${nextRelease.version}",
-    //     failComment:
-    //       "This release from branch ${branch.name} had failed due to the following errors:\n- ${errors.map(err => err.message).join('\\n- ')}",
-    //     releasedLabels: [
-    //       'released<%= nextRelease.channel ? ` on @${nextRelease.channel}` : "" %> from <%= branch.name %>',
-    //     ],
-    //   },
-    // ],
+    '@semantic-release/npm',
     [
       '@semantic-release/git',
       {
         assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
-        message: `chore(release): ${appName}-v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
+        message: `chore(release): ${libName}-v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
       },
     ],
   ],
