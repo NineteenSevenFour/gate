5bebe60b86734456e66d5622a537f60fb1b7ed9e
Bruno DUVAL <bruno.duval@datatunning.com>
2023-05-01 18:52:56 +0200

    ci(gatehub): update coverage output folder

--- libs/gatehub-typings/jest.config.ts
+++ libs/gatehub-typings/jest.config.ts
@@ -7,5 +7,5 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory:
-    '../../coverage/libs/gatehub-typings',
+    '../../testresults/libs/gatehub-typings',
 };
